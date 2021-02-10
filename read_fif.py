e9ab2478e09c06291e5bdc91240d5c6a5e016a55
AuroreBussalb <aurore.bussalb@gmail.com>
2021-02-10 10:31:16 +0100

    raise an exception and store it in product.json

--- read_fif.py
+++ read_fif.py
@@ -20,14 +20,23 @@ raw = mne.io.read_raw_fif(data_file, allow_maxshield=True)
 # Test param
 test_param = config.pop('param_test')
 if test_param is not None:
-    destination_file = str(test_param)
-else:
+    test_param = str(test_param)
+    print('a')
+elif test_param == "":
     dict_json_product['brainlife'].append({'type': 'warning', 'msg': "no param"})
 
 if test_param == "ok":
     dict_json_product['brainlife'].append({'type': 'warning', 'msg': "ok"})
 elif test_param == "okay":
     dict_json_product['brainlife'].append({'type': 'warning', 'msg': "okay"})
+else:
+    ValueError_message = f'Wrong parameter!'
+    # Add message to product.json and write it before raising the exception
+    dict_json_product['brainlife'].append({'type': 'error', 'msg': ValueError_message})
+    with open('product.json', 'w') as outfile:
+        json.dump(dict_json_product, outfile)
+    # Raise exception
+    raise ValueError(ValueError_message)
 
 # Save file
 # raw.save(raw.filenames[0].replace('.fif', 'test-raw.fif'), overwrite=True)
@@ -39,7 +48,7 @@ report = mne.Report(title='Rapport test', verbose=True)
 report.save('out_dir/report_maxfilter.html', overwrite=True)
 
 # Success message in product.json
-dict_json_product['brainlife'].append({'type': 'success', 'msg': 'MaxFilter was applied successfully.'})
+dict_json_product['brainlife'].append({'type': 'success', 'msg': 'It runs!'})
 
 # Save the dict_json_product in a json file
 with open('product.json', 'w') as outfile:
