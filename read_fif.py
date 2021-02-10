2bdf9e6f015bfe498f2fc1491de2d7d2a6d81eb7
AuroreBussalb <aurore.bussalb@gmail.com>
2021-02-10 15:27:57 +0100

    add maxfilter function

--- read_fif.py
+++ read_fif.py
@@ -2,10 +2,8 @@
 
 import mne
 import json
-import os
+import warnings
 
-# Print mne version
-print(mne.__version__)
 
 # Generate a json.product to display messages on Brainlife UI
 dict_json_product = {'brainlife': []}
@@ -18,37 +16,48 @@ with open('config.json') as config_json:
 data_file = str(config.pop('fif'))
 raw = mne.io.read_raw_fif(data_file, allow_maxshield=True)
 
-# Test param
-# test_param = config.pop('param_test')
-# if test_param is not None:
-#     test_param = str(test_param)
-#     print('a')
-# elif test_param == "":
-#     dict_json_product['brainlife'].append({'type': 'warning', 'msg': "no param"})
-#
-# if test_param == "ok":
-#     dict_json_product['brainlife'].append({'type': 'warning', 'msg': "ok"})
-# elif test_param == "okay":
-#     dict_json_product['brainlife'].append({'type': 'warning', 'msg': "okay"})
-# else:
-#     ValueError_message = f'Wrong parameter!'
-#     # Raise exception
-#     raise ValueError(ValueError_message)
+# Warning if bad channels are empty
+if raw.info['bads'] is None:
+    UserWarning_message = f'No channels are marked as bad. ' \
+                      f'Make sure to check (automatically or visually) for bad channels before ' \
+                      f'running MaxFilter.'
+    warnings.warn(UserWarning_message)
+    dict_json_product['brainlife'].append({'type': 'warning', 'msg': UserWarning_message})
+
+# Check if MaxFilter was already applied on the data
+sss_info = raw.info['proc_history'][0]['max_info']['sss_info']
+tsss_info = raw.info['proc_history'][0]['max_info']['max_st']
+if bool(sss_info) or bool(tsss_info) is True:
+    ValueError_message = f'You cannot apply MaxFilter if data have already ' \
+                     f'processed with Maxwell-filter.'
+    # Raise exception
+    raise ValueError(ValueError_message)
+
+# Apply MaxFilter
+raw_maxfilter = mne.preprocessing.maxwell_filter(raw,
+                                                 st_duration=config['param_st_duration'],
+                                                 st_correlation=config['param_st_correlation'])
+
+# Save file
+if config['param_st_duration'] is not None:
+    raw_maxfilter.save("out_dir/test-raw_tsss.fif)", overwrite=True)
+else:
+    raw_maxfilter.save("out_dir/test-raw_sss.fif)", overwrite=True)
 
 # Save file
 # raw.save(raw.filenames[0].replace('.fif', 'test-raw.fif'), overwrite=True)
 # print(raw.filenames[0].replace('.fif', 'test-raw.fif'))
-test = raw.filenames[0].replace('.fif', '_%s.fif' % config['param_output_tag'])
-# path = "/network/lustre/iss01/home/aurore.bussalb/Repositories/app-maxfilter/data/"
-path = "/out_dir/"
-path_fif = os.path.join(path, test)
-raw.save(path_fif, overwrite=True)
-
-# Generate a report
-report = mne.Report(title='Rapport test', verbose=True)
-
-# Save report
-report.save('out_dir/report_maxfilter.html', overwrite=True)
+# test = raw.filenames[0].replace('.fif', '_%s.fif' % config['param_output_tag'])
+# # path = "/network/lustre/iss01/home/aurore.bussalb/Repositories/app-maxfilter/data/"
+# path = "/out_dir/"
+# path_fif = os.path.join(path, test)
+# raw.save(path_fif, overwrite=True)
+#
+# # Generate a report
+# report = mne.Report(title='Rapport test', verbose=True)
+#
+# # Save report
+# report.save('out_dir/report_maxfilter.html', overwrite=True)
 
 # Success message in product.json
 dict_json_product['brainlife'].append({'type': 'success', 'msg': 'It runs!'})
