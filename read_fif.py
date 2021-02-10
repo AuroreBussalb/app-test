bdf972ba7e410e1e41bc67c568f33decf3985db7
AuroreBussalb <aurore.bussalb@gmail.com>
2021-02-10 11:43:13 +0100

    test save fif in out_dir

--- read_fif.py
+++ read_fif.py
@@ -18,28 +18,26 @@ data_file = str(config.pop('fif'))
 raw = mne.io.read_raw_fif(data_file, allow_maxshield=True)
 
 # Test param
-test_param = config.pop('param_test')
-if test_param is not None:
-    test_param = str(test_param)
-    print('a')
-elif test_param == "":
-    dict_json_product['brainlife'].append({'type': 'warning', 'msg': "no param"})
-
-if test_param == "ok":
-    dict_json_product['brainlife'].append({'type': 'warning', 'msg': "ok"})
-elif test_param == "okay":
-    dict_json_product['brainlife'].append({'type': 'warning', 'msg': "okay"})
-else:
-    ValueError_message = f'Wrong parameter!'
-    # Add message to product.json and write it before raising the exception
-    # dict_json_product['brainlife'].append({'type': 'error', 'msg': ValueError_message})
-    # with open('product.json', 'w') as outfile:
-    #     json.dump(dict_json_product, outfile)
-    # Raise exception
-    raise ValueError(ValueError_message)
+# test_param = config.pop('param_test')
+# if test_param is not None:
+#     test_param = str(test_param)
+#     print('a')
+# elif test_param == "":
+#     dict_json_product['brainlife'].append({'type': 'warning', 'msg': "no param"})
+#
+# if test_param == "ok":
+#     dict_json_product['brainlife'].append({'type': 'warning', 'msg': "ok"})
+# elif test_param == "okay":
+#     dict_json_product['brainlife'].append({'type': 'warning', 'msg': "okay"})
+# else:
+#     ValueError_message = f'Wrong parameter!'
+#     # Raise exception
+#     raise ValueError(ValueError_message)
 
 # Save file
 # raw.save(raw.filenames[0].replace('.fif', 'test-raw.fif'), overwrite=True)
+# print(raw.filenames[0].replace('.fif', 'test-raw.fif'))
+raw.save("out_dir/test-raw.fif", overwrite=True)
 
 # Generate a report
 report = mne.Report(title='Rapport test', verbose=True)
