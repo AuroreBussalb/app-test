16432f3f7c2815a4c6c9095ecece69467db645e6
AuroreBussalb <aurore.bussalb@gmail.com>
2021-02-11 16:40:00 +0100

    test if maxfilter was applied

--- read_fif.py
+++ read_fif.py
@@ -11,43 +11,43 @@ dict_json_product = {'brainlife': []}
 # Load inputs from config.json
 with open('config.json') as config_json:
     config = json.load(config_json)
-
-# Read all raw files and store them in a list
-keys = list(config.keys())
-list_raw = []
-for i in range(len(keys)):
-    data_file = str(config.pop(keys[i]))
-    raw = mne.io.read_raw_fif(data_file, allow_maxshield=True)
-    list_raw.append(raw)
-
-# Create an empty 3D matrix that will contain for each file its transposition matrix
-pos = np.zeros((len(keys), 4, 4))
-
-# Loop to store the transposition matrix of each file
-for raw, i in zip(list_raw, range(len(keys))):
-    pos[i] = raw.info["dev_head_t"]["trans"]
-
-# Create info object of an empty .fif file from info of the first run
-ch_names = list_raw[0].info["ch_names"]
-sfreq = list_raw[0].info["sfreq"]
-mean_tm_info = mne.create_info(ch_names, sfreq=sfreq)
-
-# Compute the mean of all matrices across the files and store it in mean_tm_info
-mean_tm_info["dev_head_t"]["trans"] = np.mean(pos, axis=0)
-
-# Create data
-data = np.ones([len(ch_names), 1])
-
-# Create raw object
-mean_tm_raw = mne.io.RawArray(data, mean_tm_info)
-mean_tm_raw.save('out_dir/mean_tm-raw.fif', overwrite=True)
+#
+# # Read all raw files and store them in a list
+# keys = list(config.keys())
+# list_raw = []
+# for i in range(len(keys)):
+#     data_file = str(config.pop(keys[i]))
+#     raw = mne.io.read_raw_fif(data_file, allow_maxshield=True)
+#     list_raw.append(raw)
+#
+# # Create an empty 3D matrix that will contain for each file its transposition matrix
+# pos = np.zeros((len(keys), 4, 4))
+#
+# # Loop to store the transposition matrix of each file
+# for raw, i in zip(list_raw, range(len(keys))):
+#     pos[i] = raw.info["dev_head_t"]["trans"]
+#
+# # Create info object of an empty .fif file from info of the first run
+# ch_names = list_raw[0].info["ch_names"]
+# sfreq = list_raw[0].info["sfreq"]
+# mean_tm_info = mne.create_info(ch_names, sfreq=sfreq)
+#
+# # Compute the mean of all matrices across the files and store it in mean_tm_info
+# mean_tm_info["dev_head_t"]["trans"] = np.mean(pos, axis=0)
+#
+# # Create data
+# data = np.ones([len(ch_names), 1])
+#
+# # Create raw object
+# mean_tm_raw = mne.io.RawArray(data, mean_tm_info)
+# mean_tm_raw.save('out_dir/mean_tm-raw.fif', overwrite=True)
 
 
 
 
 # Read the files
-# data_file = str(config.pop('fif'))
-# raw = mne.io.read_raw_fif(data_file, allow_maxshield=True)
+data_file = str(config.pop('fif'))
+raw = mne.io.read_raw_fif(data_file, allow_maxshield=True)
 
 # Warning if bad channels are empty
 # if raw.info['bads'] is None:
@@ -58,8 +58,8 @@ mean_tm_raw.save('out_dir/mean_tm-raw.fif', overwrite=True)
 #     dict_json_product['brainlife'].append({'type': 'warning', 'msg': UserWarning_message})
 
 # Check if MaxFilter was already applied on the data
-# sss_info = raw.info['proc_history'][0]['max_info']['sss_info']
-# tsss_info = raw.info['proc_history'][0]['max_info']['max_st']
+sss_info = raw.info['proc_history'][0]['max_info']['sss_info']
+tsss_info = raw.info['proc_history'][0]['max_info']['max_st']
 # if bool(sss_info) or bool(tsss_info) is True:
 #     ValueError_message = f'You cannot apply MaxFilter if data have already ' \
 #                      f'processed with Maxwell-filter.'
