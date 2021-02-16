4c9284c166a5160d710dbd14ee7f708003360918
AuroreBussalb <aurore.bussalb@gmail.com>
2021-02-16 12:06:52 +0100

    test head pos

--- read_fif.py
+++ read_fif.py
@@ -6,6 +6,9 @@ import numpy as np
 import warnings
 
 
+import mne
+import json
+
 # Generate a json.product to display messages on Brainlife UI
 dict_json_product = {'brainlife': []}
 
@@ -13,50 +16,25 @@ dict_json_product = {'brainlife': []}
 with open('config.json') as config_json:
     config = json.load(config_json)
 
-# Read all raw files and store them in a list
-# keys = list(config.keys())
-# list_raw = []
-# for i in range(len(keys)):
-#     data_file = str(config.pop(keys[i]))
-#     raw = mne.io.read_raw_fif(data_file, allow_maxshield=True)
-#     list_raw.append(raw)
+# Get cHPI
+data_file = str(config.pop('fif'))
+raw = mne.io.read_raw_fif(data_file, allow_maxshield=True)  # raw file must contain cHPI info
+chpi_amplitudes = mne.chpi.compute_chpi_amplitudes(raw)
+chpi_locs = mne.chpi.compute_chpi_locs(raw.info, chpi_amplitudes)
+head_pos = mne.chpi.compute_head_pos(raw.info, chpi_locs)
 
-list_raw = []
-for data_file in config["fif"]:
-    raw = mne.io.read_raw_fif(data_file, allow_maxshield=True)
-    list_raw.append(raw)
-
-
-# Create an empty 3D matrix that will contain for each file its transposition matrix
-pos = np.zeros((len(list_raw), 4, 4))
-
-# Loop to store the transposition matrix of each file
-for raw, i in zip(list_raw, range(len(list_raw))):
-    pos[i] = raw.info["dev_head_t"]["trans"]
-
-# Create info object of an empty .fif file from info of the first run
-ch_names = list_raw[0].info["ch_names"]
-sfreq = list_raw[0].info["sfreq"]
-mean_tm_info = mne.create_info(ch_names, sfreq=sfreq)
-
-# Compute the mean of all matrices across the files and store it in mean_tm_info
-mean_tm_info["dev_head_t"]["trans"] = np.mean(pos, axis=0)
-
-# Create data
-data = np.ones([len(ch_names), 1])
-
-# Create raw object
-mean_tm_raw = mne.io.RawArray(data, mean_tm_info)
-mean_tm_raw.save('out_dir/mean_tm-raw.fif', overwrite=True)
+# Save file
+mne.chpi.write_head_pos("out_dir/head_pos.pos", head_pos)
 
 # Success message in product.json
 dict_json_product['brainlife'].append({'type': 'success',
-                                       'msg': 'Mean transformation matrix was computed successfully.'})
+                                       'msg': 'head position file was written successfully.'})
+
+raw.save("out_dir_fif/test-head-pos-raw.fif", overwrite=True)
 
 # Save the dict_json_product in a json file
 with open('product.json', 'w') as outfile:
     json.dump(dict_json_product, outfile)
-
 
 
 
