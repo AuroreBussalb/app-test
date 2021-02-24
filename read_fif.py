72ac6f11052d68498ef67ac1377b0e3e458cbed6
AuroreBussalb <aurore.bussalb@gmail.com>
2021-02-24 15:29:55 +0100

    test head pos app

--- read_fif.py
+++ read_fif.py
@@ -1,8 +1,5 @@
-#!/usr/local/bin/python3
-
-import json
 import mne
-import warnings
+import json
 
 # Generate a json.product to display messages on Brainlife UI
 dict_json_product = {'brainlife': []}
@@ -11,96 +8,19 @@ dict_json_product = {'brainlife': []}
 with open('config.json') as config_json:
     config = json.load(config_json)
 
-# Read the files
-data_file = config.pop('fif')
-raw = mne.io.read_raw_fif(data_file, allow_maxshield=True)
+# Get cHPI
+data_file = str(config.pop('fif'))
+raw = mne.io.read_raw_fif(data_file, allow_maxshield=True)  # raw file must contain cHPI info
+chpi_amplitudes = mne.chpi.compute_chpi_amplitudes(raw)
+chpi_locs = mne.chpi.compute_chpi_locs(raw.info, chpi_amplitudes)
+head_pos = mne.chpi.compute_head_pos(raw.info, chpi_locs)
 
-# Read the calibration files
-if 'cross_talk_correction' in config.keys():
-    cross_talk_file = config.pop('cross_talk_correction')
-else:
-    cross_talk_file = None
-
-if 'calibration' in config.keys():
-    calibration_file = config.pop('calibration')
-else:
-    calibration_file = None
-
-# Read the run to realign all runs
-if 'destination' in config.keys():
-    destination_file = config.pop('destination')
-else:
-    destination_file = None
-
-# Head pos file
-if 'head_position' in config.keys():
-    head_pos_file = config.pop('head_position')
-    if head_pos_file is not None:  # when App is run locally and "head_position": null in config.json
-        head_pos_file = mne.chpi.read_head_pos(head_pos_file)
-else:
-    head_pos_file = None
-
-# Warning if bad channels are empty
-if not raw.info['bads']:
-    UserWarning_message = f'No channels are marked as bad. ' \
-                          f'Make sure to check (automatically or visually) for bad channels before ' \
-                          f'running MaxFilter.'
-    warnings.warn(UserWarning_message)
-    dict_json_product['brainlife'].append({'type': 'warning', 'msg': UserWarning_message})
-
-# Check if MaxFilter was already applied on the data
-if raw.info['proc_history']:
-    sss_info = raw.info['proc_history'][0]['max_info']['sss_info']
-    tsss_info = raw.info['proc_history'][0]['max_info']['max_st']
-    if bool(sss_info) or bool(tsss_info) is True:
-        ValueError_message = f'You cannot apply MaxFilter if data have already ' \
-                             f'processed with Maxwell filtering.'
-        # Raise exception
-        raise ValueError(ValueError_message)
-
-# Apply MaxFilter
-raw_maxfilter = mne.preprocessing.maxwell_filter(raw, calibration=calibration_file, cross_talk=cross_talk_file,
-                                                 head_pos=head_pos_file, destination=destination_file,
-                                                 st_duration=config['param_st_duration'],
-                                                 st_correlation=config['param_st_correlation'],
-                                                 int_order=config['param_int_order'],
-                                                 ext_order=config['param_ext_order'],
-                                                 coord_frame=config['param_coord_frame'],
-                                                 regularize=config['param_regularize'],
-                                                 ignore_ref=config['param_ignore_ref'],
-                                                 bad_condition=config['param_bad_condition'],
-                                                 st_fixed=config['param_st_fixed'],
-                                                 st_only=config['param_st_only'],
-                                                 skip_by_annotation=config['param_skip_by_annotation'],
-                                                 mag_scale=config['param_mag_scale'])
 # Save file
-if config['param_st_duration'] is not None:
-    raw_maxfilter.save("out_dir_maxfilter/test-raw_tsss.fif", overwrite=True)
-else:
-    raw_maxfilter.save("out_dir_maxfilter/test-raw_sss.fif", overwrite=True)
-
-# Generate a report
-report = mne.Report(title='Results Maxfilter', verbose=True)
-
-# Plot MEG signals in temporal domain
-fig_raw = raw.pick(['meg']).plot(duration=10, butterfly=False, show_scrollbars=False)
-fig_raw_maxfilter = raw_maxfilter.pick(['meg']).plot(duration=10, butterfly=False, show_scrollbars=False)
-
-# Plot power spectral density
-fig_raw_psd = raw.plot_psd()
-fig_raw_maxfilter_psd = raw_maxfilter.plot_psd()
-
-# Add figures to report
-report.add_figs_to_section(fig_raw, captions='MEG signals before MaxFilter')
-report.add_figs_to_section(fig_raw_maxfilter, captions='MEG signals after MaxFilter')
-report.add_figs_to_section(fig_raw_psd, captions='Power spectral density before MaxFilter')
-report.add_figs_to_section(fig_raw_maxfilter_psd, captions='Power spectral density after MaxFilter')
-
-# Save report
-report.save('out_dir_report/report_maxfilter.html', overwrite=True)
+mne.chpi.write_head_pos("out_dir/head_pos.pos", head_pos)
 
 # Success message in product.json
-dict_json_product['brainlife'].append({'type': 'success', 'msg': 'MaxFilter was applied successfully.'})
+dict_json_product['brainlife'].append({'type': 'success',
+                                       'msg': 'head position file was written successfully.'})
 
 # Save the dict_json_product in a json file
 with open('product.json', 'w') as outfile:
