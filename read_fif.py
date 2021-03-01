d394c57b36b7b7f5577c5bf6c53c6382a9c54255
AuroreBussalb <aurore.bussalb@gmail.com>
2021-03-01 11:59:28 +0100

    read and write files

--- read_fif.py
+++ read_fif.py
@@ -8,19 +8,15 @@ dict_json_product = {'brainlife': []}
 with open('config.json') as config_json:
     config = json.load(config_json)
 
-# Get cHPI
 data_file = str(config.pop('fif'))
-raw = mne.io.read_raw_fif(data_file, allow_maxshield=True)  # raw file must contain cHPI info
-chpi_amplitudes = mne.chpi.compute_chpi_amplitudes(raw)
-chpi_locs = mne.chpi.compute_chpi_locs(raw.info, chpi_amplitudes)
-head_pos = mne.chpi.compute_head_pos(raw.info, chpi_locs)
+raw = mne.io.read_raw_fif(data_file, allow_maxshield=True)
 
 # Save file
-mne.chpi.write_head_pos("out_dir_head_pos/head_pos.pos", head_pos)
+raw.save("out_dir/test-raw.fif", overwrite=True)
 
 # Success message in product.json
 dict_json_product['brainlife'].append({'type': 'success',
-                                       'msg': 'head position file was written successfully.'})
+                                       'msg': 'Yeah'})
 
 # Save the dict_json_product in a json file
 with open('product.json', 'w') as outfile:
