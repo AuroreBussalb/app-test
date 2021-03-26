a89604c8a167f9b36940db21f4ec64d7d282d897
aurore.bussalb <aurore.bussalb@mensiatech.com>
2021-03-26 14:42:32 +0100

    print raw info

--- read_fif.py
+++ read_fif.py
@@ -11,8 +11,7 @@ with open('config.json') as config_json:
 data_file = str(config.pop('fif'))
 raw = mne.io.read_raw_fif(data_file, allow_maxshield=True)
 
-test = data_file = str(config.pop('_inputs'))
-print(test)
+print(raw.info)
 
 # Save file
 raw.save("test-raw.fif", overwrite=True)
