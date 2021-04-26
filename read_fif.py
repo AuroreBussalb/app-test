2431d208b838868f14b60071eaf1aeaf12e54f4c
AuroreBussalb <aurore.bussalb@gmail.com>
2021-04-26 09:22:25 +0200

    test intergers

--- read_fif.py
+++ read_fif.py
@@ -6,5 +6,11 @@ import json
 import os
 import shutil
 
+# Load inputs from config.json
+with open('config.json') as config_json:
+    config = json.load(config_json)
+
+tmp = dict((k, None) for k, v in config.items() if v == "")
+config.update(tmp)
 
 print(config['list_of_integers'])
