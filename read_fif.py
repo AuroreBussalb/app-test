8d60522ec54b5d9fc89a7823713119790455d662
AuroreBussalb <aurore.bussalb@gmail.com>
2021-04-26 09:43:30 +0200

    test list of integers

--- read_fif.py
+++ read_fif.py
@@ -13,4 +13,9 @@ with open('config.json') as config_json:
 tmp = dict((k, None) for k, v in config.items() if v == "")
 config.update(tmp)
 
-print(config['list_of_integers'][0])
+picks = config['list_of_integers']
+if isinstance(picks, str) and picks.find("[") != -1 and picks is not None:
+    picks = picks.replace('[', '')
+    picks = picks.replace(']', '')
+    picks = picks.replace("'", '')
+    config['list_of_integers'] = list(map(str, picks.split(', ')))
