e9e5ec0e69687d3b8c192fd0561f9a9e97a4a0ed
AuroreBussalb <aurore.bussalb@gmail.com>
2021-04-28 11:17:55 +0200

    test convert ctf files

--- read_fif.py
+++ read_fif.py
@@ -10,15 +10,27 @@ import shutil
 with open('config.json') as config_json:
     config = json.load(config_json)
 
-tmp = dict((k, None) for k, v in config.items() if v == "")
-config.update(tmp)
+# Read ctf file
+data_file = config.pop('ds')
+raw_ctf = mne.io.read_raw_ctf(data_file)
+raw_ctf.save('test_raw.fif')
 
-picks = config['list_of_integers']
-print(picks)
-if isinstance(picks, str) and picks.find("[") != -1 and picks is not None:
-    picks = picks.replace('[', '')
-    picks = picks.replace(']', '')
-    picks = picks.replace("'", '')
-    config['list_of_integers'] = list(map(str, picks.split(', ')))
-print(config['list_of_integers'])
-print(config['list_of_integers'][0])
+# list_keys = list(config['reject'].keys())
+# list_keys = [k[:-1] for k in list_keys] 
+# print(list_keys)
+
+# test = dict((k.replace("'", ""), v) for k, v in config['reject'].items())
+# print(test)
+
+# tmp = dict((k, None) for k, v in config.items() if v == "")
+# config.update(tmp)
+
+# picks = config['list_of_integers']
+# print(picks)
+# if isinstance(picks, str) and picks.find("[") != -1 and picks is not None:
+#     picks = picks.replace('[', '')
+#     picks = picks.replace(']', '')
+#     picks = picks.replace("'", '')
+#     config['list_of_integers'] = list(map(str, picks.split(', ')))
+# print(config['list_of_integers'])
+# print(config['list_of_integers'][0])
