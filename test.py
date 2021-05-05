c5db063238947fd94313cdb51a118e6468eb4998
aurore.bussalb <aurore.bussalb@mensiatech.com>
2021-05-05 11:21:08 +0200

    read file in bids directory

--- test.py
+++ test.py
@@ -11,6 +11,6 @@ with open('config.json') as config_json:
 # Read files frim bids directory
 
 data_file = config.pop('ds')
-raw_ctf = mne.io.read_raw('bids/sub-0001/meg/sub-0001_task-AEF_run-02_meg.ds/sub-0001_task-AEF_run-02_meg.res4')
+raw_ctf = mne.io.read_raw_ctf('bids/sub-0001/meg/sub-0001_task-AEF_run-02_meg.ds/sub-0001_task-AEF_run-02_meg.res4')
 raw_ctf.save('test_raw.fif')
 
