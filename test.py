4395112e58f74d7a7d0bb26922b044fb9a0ed593
AuroreBussalb <aurore.bussalb@gmail.com>
2021-05-11 17:08:57 +0200

    test config

--- test.py
+++ test.py
@@ -14,32 +14,32 @@ tmp = dict((k, None) for k, v in config.items() if v == "")
 config.update(tmp)
 
 # Read files from bids directory
-# bids_path = BIDSPath(subject=config['subject'],
-#                      session=config['session'],
-#                      task=config['task'],
-#                      run=config['run'],
-#                      acquisition=config['acquisition'],
-#                      processing=config['processing'],
-#                      recording=config['recording'],
-#                      space=config['space'],
-#                      suffix=config['suffix'],
-#                      datatype='meg',
-#                      root='bids')
-
-bids_path = BIDSPath(subject="0001",
-                     session=None,
-                     task="AEF",
-                     run="02",
-                     acquisition=None,
-                     processing=None,
-                     recording=None,
-                     space=None,
-                     suffix=None,
+bids_path = BIDSPath(subject=config['subject'],
+                     session=config['session'],
+                     task=config['task'],
+                     run=config['run'],
+                     acquisition=config['acquisition'],
+                     processing=config['processing'],
+                     recording=config['recording'],
+                     space=config['space'],
+                     suffix=config['suffix'],
                      datatype='meg',
                      root='bids')
 
-# raw_ctf = read_raw_bids(bids_path)
+# bids_path = BIDSPath(subject="0001",
+#                      session=None,
+#                      task="AEF",
+#                      run="02",
+#                      acquisition=None,
+#                      processing=None,
+#                      recording=None,
+#                      space=None,
+#                      suffix=None,
+#                      datatype='meg',
+#                      root='bids')
 
-# # Save ctf file into fif
-# raw_ctf.save('out_dir/meg.fif', overwrite=True)
+raw_ctf = read_raw_bids(bids_path)
+
+# Save ctf file into fif
+raw_ctf.save('out_dir/meg.fif', overwrite=True)
 
