7ae3a152153aa27862fe8b707771311d7637abad
AuroreBussalb <aurore.bussalb@gmail.com>
2021-05-17 10:00:24 +0200

    save ctf file

--- test.py
+++ test.py
@@ -9,37 +9,44 @@ from mne_bids import BIDSPath, read_raw_bids
 with open('config.json') as config_json:
     config = json.load(config_json)
 
-# Convert all "" into None when the App runs on BL 
-tmp = dict((k, None) for k, v in config.items() if v == "")
-config.update(tmp)
+# Read ctf
+data_file = config.pop('ctf')
+raw = mne.io.read_raw(data_file, allow_maxshield=True)
 
-# Read files from bids directory
-bids_path = BIDSPath(subject=config['subject'],
-                     session=config['session'],
-                     task=config['task'],
-                     run=config['run'],
-                     acquisition=config['acquisition'],
-                     processing=config['processing'],
-                     recording=config['recording'],
-                     space=config['space'],
-                     suffix=config['suffix'],
-                     datatype='meg',
-                     root='bids')
+# Save ctf file
+mean_tm_raw.save('out_dir/test.ctf', overwrite=True)
 
-# bids_path = BIDSPath(subject="0001",
-#                      session=None,
-#                      task="AEF",
-#                      run="02",
-#                      acquisition=None,
-#                      processing=None,
-#                      recording=None,
-#                      space=None,
-#                      suffix=None,
+
+# Read the raw file
+# data_file = config.pop('fif')
+# raw = mne.io.read_raw_fif(data_file, allow_maxshield=True)   
+
+# print(raw.pick_types(eeg=True)) 
+# print(raw.info['ch_names']) 
+
+
+# print(raw.info['chs'])
+
+# # Convert all "" into None when the App runs on BL 
+# tmp = dict((k, None) for k, v in config.items() if v == "")
+# config.update(tmp)
+
+# # Create BIDS directory
+# bids_path = BIDSPath(subject=config['subject'],
+#                      session=config['session'],
+#                      task=config['task'],
+#                      run=config['run'],
+#                      acquisition=config['acquisition'],
+#                      processing=config['processing'],
+#                      recording=config['recording'],
+#                      space=config['space'],
+#                      suffix=config['suffix'],
 #                      datatype='meg',
 #                      root='bids')
 
-raw_ctf = read_raw_bids(bids_path)
+# # Read file from BIDS directory
+# raw_ctf = read_raw_bids(bids_path)
 
-# Save ctf file into fif
-raw_ctf.save('out_dir/meg.fif', overwrite=True)
+# # Save ctf file into fif
+# raw_ctf.save('out_dir/meg.fif', overwrite=True)
 
